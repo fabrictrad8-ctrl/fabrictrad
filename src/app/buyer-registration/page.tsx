08087c184181b5b62a0ba067bbd4f6261df147fe
fabrictrad8-ctrl <fabrictrad8@gmail.com>
2026-07-31 23:09:40 +0530

    Route buyer registration through buyer-type selection

--- src/app/buyer-registration/page.tsx
+++ src/app/buyer-registration/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from '@/components/Header';
-import BuyerRegistrationFlow from '@/app/buyer-registration/components/BuyerRegistrationFlow';
+import BuyerRegistrationEntry from '@/app/buyer-registration/components/BuyerRegistrationEntry';
 
 export default function BuyerRegistrationPage() {
   return (
     <main className="ft-storefront min-h-screen">
       <Header />
       <div className="pt-16">
-        <BuyerRegistrationFlow />
+        <BuyerRegistrationEntry />
       </div>
     </main>
   );
