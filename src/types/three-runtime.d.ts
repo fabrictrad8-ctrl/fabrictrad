02defcb4a2b1b54f3414ae22f48aee684789ff6e
fabrictrad8-ctrl <fabrictrad8@gmail.com>
2026-08-22 01:58:22 +0530

    type dynamic Three.js runtime imports

---
+++ src/types/three-runtime.d.ts
@@ -0,0 +1,6 @@
+declare namespace THREE {
+  type BufferGeometry = import('three').BufferGeometry;
+  type Material = import('three').Material;
+  type MeshStandardMaterial = import('three').MeshStandardMaterial;
+  type Texture = import('three').Texture;
+}
