a10f6ebd386aa67ce844140669466617c4461850
fabrictrad8-ctrl <fabrictrad8@gmail.com>
2026-08-25 02:25:21 +0530

    Adopt search-first marketplace page structure

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -11,18 +11,16 @@ import BuyerOnlyGuard from '@/components/BuyerOnlyGuard';
 export default function MarketplacePage() {
   return (
     <BuyerOnlyGuard>
-      <main className="ft-storefront min-h-screen">
+      <main className="ft-storefront ft-marketplace-shell min-h-screen">
         <MarketplaceQueryBridge />
         <Header />
         <div className="pt-16">
           <MarketplaceBanner />
-          <div className="ft-storefront-content py-5 sm:py-7">
+          <div className="ft-storefront-content py-4 sm:py-5">
             <MarketplaceActiveFilters />
-            <div className="flex flex-col gap-5 lg:flex-row lg:items-start lg:gap-6">
+            <div className="flex flex-col gap-4 lg:flex-row lg:items-start lg:gap-5">
               <MarketplaceFilters />
-              <div className="min-w-0 flex-1">
-                <MarketplaceGrid />
-              </div>
+              <div className="min-w-0 flex-1"><MarketplaceGrid /></div>
             </div>
           </div>
         </div>
