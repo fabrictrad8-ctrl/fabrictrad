f293622d2e1a64ac8e38fcb82e19defbe996473d
fabrictrad8-ctrl <fabrictrad8@gmail.com>
2026-09-01 07:29:29 +0530

    Harden buyer store normalization function search paths

---
+++ supabase/migrations/20260901015913_harden_buyer_store_normalization_functions.sql
@@ -0,0 +1,3 @@
+ALTER FUNCTION public.fabrictrad_store_key(text) SET search_path = '';
+ALTER FUNCTION public.fabrictrad_store_handle(text) SET search_path = '';
+ALTER FUNCTION public.buyer_stores_normalize() SET search_path = '';
